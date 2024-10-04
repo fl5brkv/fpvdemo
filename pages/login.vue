ff7f3cd3c104aaecba34a4efdee4c59f7d8a10c2
fl5brkv <fl5brkv@proton.me>
2024-10-04 12:08:01 +0200

    updated login.vue

--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,39 @@
 <template>
-login
+  <form @submit.prevent="onSubmit">
+    <input v-model="form.email" type="email" placeholder="Email" />
+    <input
+      v-model="form.plaintextPassword"
+      type="password"
+      placeholder="Password" />
+    <button type="submit" :disabled="loading">Login</button>
+  </form>
+  <!-- Show error message if there is an error -->
+  <!-- <p v-if="error">{{ error.statusMessage }}</p>
+  <button @click="clearError">This will clear the error.</button> -->
 </template>
 
 <script setup lang="ts">
+// Form data
+const form = ref({
+  email: '',
+  plaintextPassword: '',
+});
 
+// Error and loading states
+const error = ref(null);
+const loading = ref(false);
+
+const onSubmit = async () => {
+  loading.value = true
+  try {
+    await $fetch('/api/auth/login', {
+      method: 'POST',
+      body: form.value,
+    });
+  } catch (e: any) {
+    console.error('ti jebe maženo' + e.statusCode)
+  } finally {
+    loading.value = false
+  }
+};
 </script>
