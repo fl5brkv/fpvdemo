30e3495f3d6ca70e64e35501cb2815b11b5ff6ed
fl5brkv <fl5brkv@proton.me>
2025-03-28 17:18:14 +0100

    small change

--- app/pages/flights/index.vue
+++ app/pages/flights/index.vue
@@ -14,48 +14,50 @@
     No flights available.
   </div>
 
-  <div
-    v-for="flight in flights"
-    :key="flight.flightId"
-    class="p-6 rounded-lg border border-gray-200 mb-4">
-    <!-- Flight Info -->
-    <h2 class="text-2xl font-semibold text-gray-700 truncate mb-4">
-      {{ flight.flightName }}
-    </h2>
-    <p class="text-sm text-gray-500 mb-2" v-show="flight.location">
-      Flight location
-      <span class="font-semibold">{{ flight.location }}</span>
-    </p>
-    <p class="text-sm text-gray-500 mb-2" v-show="flight.date">
-      Date:
-      <span class="font-semibold">{{ formatDate(flight.date) }}</span>
-    </p>
-    <p class="text-sm text-gray-500 mb-2" v-show="flight.landings">
-      Landings:
-      <span class="font-semibold">{{ flight.landings }}</span>
-    </p>
-    <p class="text-sm text-gray-500 mb-2" v-show="flight.timeInAir">
-      Time in Air:
-      <span class="font-semibold">{{ flight.timeInAir }} minutes</span>
-    </p>
-    <p class="text-sm text-gray-500 mb-2" v-show="flight.additionalInfo">
-      Additional info
-      <span class="font-semibold">{{ flight.additionalInfo }}</span>
-    </p>
+  <div class="grid lg:grid-cols-2 xl:grid-cols-3 gap-5">
+    <div
+      v-for="flight in flights"
+      :key="flight.flightId"
+      class="p-6 rounded-lg border border-gray-200 mb-4">
+      <!-- Flight Info -->
+      <h2 class="text-2xl font-semibold text-gray-700 truncate mb-4">
+        {{ flight.flightName }}
+      </h2>
+      <p class="text-sm text-gray-500 mb-2" v-show="flight.location">
+        Flight location
+        <span class="font-semibold">{{ flight.location }}</span>
+      </p>
+      <p class="text-sm text-gray-500 mb-2" v-show="flight.date">
+        Date:
+        <span class="font-semibold">{{ formatDate(flight.date) }}</span>
+      </p>
+      <p class="text-sm text-gray-500 mb-2" v-show="flight.landings">
+        Landings:
+        <span class="font-semibold">{{ flight.landings }}</span>
+      </p>
+      <p class="text-sm text-gray-500 mb-2" v-show="flight.timeInAir">
+        Time in Air:
+        <span class="font-semibold">{{ flight.timeInAir }} minutes</span>
+      </p>
+      <p class="text-sm text-gray-500 mb-2" v-show="flight.additionalInfo">
+        Additional info
+        <span class="font-semibold">{{ flight.additionalInfo }}</span>
+      </p>
 
-    <!-- Actions -->
-    <div class="flex justify-between items-center">
-      <NuxtLink
-        :to="`/flights/update/${flight.flightId}`"
-        class="text-blue-600 hover:text-blue-800 text-sm font-medium">
-        Edit ✏️
-      </NuxtLink>
+      <!-- Actions -->
+      <div class="flex justify-between items-center">
+        <NuxtLink
+          :to="`/flights/update/${flight.flightId}`"
+          class="text-blue-600 hover:text-blue-800 text-sm font-medium">
+          Edit ✏️
+        </NuxtLink>
 
-      <button
-        @click="deleteFlight({flightId: flight.flightId})"
-        class="text-red-600 hover:text-red-800 text-sm font-medium">
-        Delete 🗑️
-      </button>
+        <button
+          @click="deleteFlight({flightId: flight.flightId})"
+          class="text-red-600 hover:text-red-800 text-sm font-medium">
+          Delete 🗑️
+        </button>
+      </div>
     </div>
   </div>
 </template>
--- app/pages/items/index.vue
+++ app/pages/items/index.vue
@@ -14,40 +14,42 @@
     No items available.
   </div>
 
-  <div
-    v-for="item in items"
-    :key="item.itemId"
-    class="p-6 rounded-lg border border-gray-200 mb-4">
-    <!-- Item Info -->
-    <div class="flex justify-between mb-4">
-      <h2 class="text-2xl font-semibold text-gray-700 truncate">
-        {{ item.itemName }}
-      </h2>
-      <span class="text-sm font-medium px-2 py-1 rounded">
-        {{ item.category }}
-      </span>
-    </div>
-    <p class="text-sm text-gray-500 mb-2" v-show="item.status">
-      Status: <span class="font-semibold">{{ item.status }}</span>
-    </p>
-    <p class="text-sm text-gray-500 mb-4" v-show="item.additionalInfo">
-      Additional Info:
-      <span class="font-semibold">{{ item.additionalInfo || 'N/A' }}</span>
-    </p>
+  <div class="grid lg:grid-cols-2 xl:grid-cols-3 gap-5">
+    <div
+      v-for="item in items"
+      :key="item.itemId"
+      class="p-6 rounded-lg border border-gray-200 mb-4">
+      <!-- Item Info -->
+      <div class="flex justify-between mb-4">
+        <h2 class="text-2xl font-semibold text-gray-700 truncate">
+          {{ item.itemName }}
+        </h2>
+        <span class="text-sm font-medium px-2 py-1 rounded">
+          {{ item.category }}
+        </span>
+      </div>
+      <p class="text-sm text-gray-500 mb-2" v-show="item.status">
+        Status: <span class="font-semibold">{{ item.status }}</span>
+      </p>
+      <p class="text-sm text-gray-500 mb-4" v-show="item.additionalInfo">
+        Additional Info:
+        <span class="font-semibold">{{ item.additionalInfo || 'N/A' }}</span>
+      </p>
 
-    <!-- Actions -->
-    <div class="flex justify-between items-center">
-      <NuxtLink
-        :to="`/items/update/${item.itemId}`"
-        class="text-blue-600 hover:text-blue-800 text-sm font-medium">
-        Edit ✏️
-      </NuxtLink>
+      <!-- Actions -->
+      <div class="flex justify-between items-center">
+        <NuxtLink
+          :to="`/items/update/${item.itemId}`"
+          class="text-blue-600 hover:text-blue-800 text-sm font-medium">
+          Edit ✏️
+        </NuxtLink>
 
-      <button
-        @click="deleteItem({itemId: item.itemId})"
-        class="text-red-600 hover:text-red-800 text-sm font-medium">
-        Delete 🗑️
-      </button>
+        <button
+          @click="deleteItem({itemId: item.itemId})"
+          class="text-red-600 hover:text-red-800 text-sm font-medium">
+          Delete 🗑️
+        </button>
+      </div>
     </div>
   </div>
 </template>
